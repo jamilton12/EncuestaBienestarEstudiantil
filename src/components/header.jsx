f7b6b0a46b7cd416826f70d385a19fd887dc3865
jamilton12 <161367374+jamilton12@users.noreply.github.com>
2025-04-09 21:40:10 -0500

    prueba

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,8 @@
 export const Header = () => {
     return (
         <header className="flex py-5 px-10 w-full bg-marian-blue/30 backdrop-filter backdrop-blur-lg fixed top-0"> 
-            <p className="text-2xl font-bold flex gap-2 flex-col">Bienestar <span>Estudiantil</span></p>
+            <p className="text-2xl font-bold flex gap-2 flex-col">Bienestar  
+                 <span>Estudiantil</span></p>
         </header>
     )
 }
